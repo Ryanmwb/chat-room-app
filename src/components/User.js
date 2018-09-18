df1d9308fa8701553226b21df51575753a925912
Ryan <ryanmwb@gmail.com>
2018-09-18 17:51:16 -0400

    finish set username checkpoint

--- src/components/User.js
+++ src/components/User.js
@@ -18,18 +18,21 @@ class User extends Component {
         const provider = new this.props.firebase.auth.GoogleAuthProvider();
         this.props.firebase.auth().signInWithPopup( provider );
         this.setState({signInOrOut: "Sign Out"})
+        console.log("sign in")
     }
 
     signOut(){
         this.props.firebase.auth().signOut();
         this.setState({signInOrOut: "Sign In"})
+        console.log("sign out")
+        console.log(this.props.user)
     }
 
     render(){
         return(
             <div>
-                <h1>User: {/* this.props.user === "Guest" ? this.props.user : this.props.user.displayName */} </h1>
-                <button onClick={ this.props.user !== 'Guest' ? this.signOut.bind(this) : this.signIn.bind(this)}>{ this.props.user !== 'Guest' ? "Sign Out" : "Sign In" }</button>
+                <h1>User: { this.props.user === null ? "Guest" : this.props.user.displayName } </h1>
+                <button onClick={ this.props.user !== null ? this.signOut.bind(this) : this.signIn.bind(this)}>{ this.props.user !== null ? "Sign Out" : "Sign In" }</button>
             </div>
         )
     }
